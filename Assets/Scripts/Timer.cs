6af0b5a9e935cb489d5b295debfb74821c7e99a6
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2024-01-09 17:52:20 +0100

    Imported TMPro & Implemented Countdown Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI timerUI;
+    private Image clockIcon;
+    
+    public float remainingTime;
+
+    public Color emergencyColor = Color.red;
+
+    private void Awake()
+    {
+        timerUI = GetComponent<TextMeshProUGUI>();
+        clockIcon = GetComponentInChildren<Image>();
+    }
+
+    private void Update()
+    {
+        if (remainingTime < 11)
+        {
+            timerUI.color = emergencyColor;
+            clockIcon.color = emergencyColor;
+        }
+        
+        if (remainingTime > 0)
+            remainingTime -= Time.deltaTime;
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            return;
+        }
+        
+        float minutes = Mathf.FloorToInt(remainingTime / 60);
+        float seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerUI.text = $"{minutes:00}:{seconds:00}";
+    }
+}
