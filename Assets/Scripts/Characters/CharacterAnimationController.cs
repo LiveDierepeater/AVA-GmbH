03ba4da8a2fff1e40e29e5fbd837de3513187ac0
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2024-01-19 13:44:44 +0100

    Update CharacterAnimationController.cs
    Added Namespace

--- Assets/Scripts/Characters/CharacterAnimationController.cs
+++ Assets/Scripts/Characters/CharacterAnimationController.cs
@@ -1,84 +1,86 @@
+using Karts;
 using UnityEngine;
 using UnityEngine.AI;
-using Characters;
-using Karts;
 using Random = UnityEngine.Random;
 
-public class CharacterAnimationController : MonoBehaviour
+namespace Characters
 {
-    public Animator unitAnimator;
-    public int randomIdlingChance;
-    
-    private SelectableUnit unit;
-    private NavMeshAgent unitAgent;
-    private GoKart currentGoKart;
-    
-    private float velocity;
-    private bool hasUnitItemInHand;
-    private bool isUnitRepairing;
-    private bool isUnitRandomIdling;
-    
-    private void Awake()
+    public class CharacterAnimationController : MonoBehaviour
     {
-        unit = GetComponent<SelectableUnit>();
-        unitAgent = GetComponent<NavMeshAgent>();
-        currentGoKart = GameObject.Find("GoKart").GetComponent<GoKart>();
-    }
-
-    private void Update()
-    {
-        if (unit.DoesUnitHaveAnythingInHand())
+        public Animator unitAnimator;
+        public int randomIdlingChance;
+    
+        private SelectableUnit unit;
+        private NavMeshAgent unitAgent;
+        private GoKart currentGoKart;
+    
+        private float velocity;
+        private bool hasUnitItemInHand;
+        private bool isUnitRepairing;
+        private bool isUnitRandomIdling;
+    
+        private void Awake()
         {
-            // Units Animation with Item in Hand.
-            hasUnitItemInHand = true;
-        }
-        else
-        {
-            // Units Animation without Item in hand.
-            hasUnitItemInHand = false;
+            unit = GetComponent<SelectableUnit>();
+            unitAgent = GetComponent<NavMeshAgent>();
+            currentGoKart = GameObject.Find("GoKart").GetComponent<GoKart>();
         }
 
-        // When Unit is doing something on GoKart.
-        if (currentGoKart.IsUnitInRange(unitAgent))
+        private void Update()
         {
-            if (unit.currentState == SelectableUnit.States.RepairKart ||
-                unit.currentState == SelectableUnit.States.AddCarComponent ||
-                unit.currentState == SelectableUnit.States.RemoveCarComponent)
+            if (unit.DoesUnitHaveAnythingInHand())
             {
-                isUnitRepairing = true;
+                // Units Animation with Item in Hand.
+                hasUnitItemInHand = true;
             }
             else
             {
-                isUnitRepairing = false;
+                // Units Animation without Item in hand.
+                hasUnitItemInHand = false;
             }
-        }
+
+            // When Unit is doing something on GoKart.
+            if (currentGoKart.IsUnitInRange(unitAgent))
+            {
+                if (unit.currentState == SelectableUnit.States.RepairKart ||
+                    unit.currentState == SelectableUnit.States.AddCarComponent ||
+                    unit.currentState == SelectableUnit.States.RemoveCarComponent)
+                {
+                    isUnitRepairing = true;
+                }
+                else
+                {
+                    isUnitRepairing = false;
+                }
+            }
         
-        if (unitAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Random"))
+            if (unitAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Random"))
+            {
+                if (unitAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.7f) isUnitRandomIdling = false;
+                if (velocity >= 0.01f) isUnitRandomIdling = false;
+            }
+        
+            velocity = unitAgent.velocity.magnitude / 10f;
+            unitAnimator.SetFloat("Speed", velocity);
+            unitAnimator.SetBool("IsItemEquipped", hasUnitItemInHand);
+            unitAnimator.SetBool("IsRepairing", isUnitRepairing);
+            unitAnimator.SetBool("IsRandomIdling", isUnitRandomIdling);
+        }
+
+        private void Start()
         {
-            if (unitAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.7f) isUnitRandomIdling = false;
-            if (velocity >= 0.01f) isUnitRandomIdling = false;
+            Invoke(nameof(RollRandomIdlingChance), 1f);
+            var state = unitAnimator.GetCurrentAnimatorStateInfo(0);
+            unitAnimator.Play(state.fullPathHash, 0, Random.Range(0f, 1f));
         }
-        
-        velocity = unitAgent.velocity.magnitude / 10f;
-        unitAnimator.SetFloat("Speed", velocity);
-        unitAnimator.SetBool("IsItemEquipped", hasUnitItemInHand);
-        unitAnimator.SetBool("IsRepairing", isUnitRepairing);
-        unitAnimator.SetBool("IsRandomIdling", isUnitRandomIdling);
-    }
 
-    private void Start()
-    {
-        Invoke(nameof(RollRandomIdlingChance), 1f);
-        var state = unitAnimator.GetCurrentAnimatorStateInfo(0);
-        unitAnimator.Play(state.fullPathHash, 0, Random.Range(0f, 1f));
-    }
+        private void RollRandomIdlingChance()
+        {
+            int roll = Random.Range(0, 101);
 
-    private void RollRandomIdlingChance()
-    {
-        int roll = Random.Range(0, 101);
+            if (randomIdlingChance > roll) isUnitRandomIdling = true;
 
-        if (randomIdlingChance > roll) isUnitRandomIdling = true;
-
-        Invoke(nameof(RollRandomIdlingChance), 1f);
+            Invoke(nameof(RollRandomIdlingChance), 1f);
+        }
     }
 }
