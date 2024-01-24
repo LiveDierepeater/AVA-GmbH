df90e948829e024c9d4c1fe3df40356de7d4c3b8
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2024-01-24 23:34:37 +0100

    Implemented Pause Sounds

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,13 +1,19 @@
-using Task;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Task;
 
 public class PauseMenu : MonoBehaviour
 {
+    public AudioClip PauseSFX;
+    public AudioClip ResumeSFX;
+    
     private GameObject pauseMenu;
+    private AudioSource audioSource;
 
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+        
         pauseMenu = transform.GetChild(0).gameObject;
         pauseMenu.SetActive(false);
     }
@@ -22,7 +28,11 @@ public class PauseMenu : MonoBehaviour
             {
                 Time.timeScale = 0;
                 pauseMenu.SetActive(true);
+                
                 TaskManager.Instance.soundManager.PauseMenuMusic();
+
+                audioSource.clip = PauseSFX;
+                audioSource.Play();
             }
         }
     }
@@ -31,7 +41,11 @@ public class PauseMenu : MonoBehaviour
     {
         Time.timeScale = 1;
         pauseMenu.SetActive(false);
+        
         TaskManager.Instance.soundManager.ResumeMusic();
+        
+        audioSource.clip = ResumeSFX;
+        audioSource.Play();
     }
 
     public void LoadMainMenu()
